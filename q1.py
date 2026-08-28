ea231a919e728f93658f7da979cc08fb025810a8
R Nikhil Ryan Jerontius <nikhilryanjerontius@gmail.com>
2026-08-28 14:26:32 +0530

    Complete Q1 List Analyzer

--- q1.py
+++ q1.py
@@ -1,1 +1,26 @@
-
+n=int(input("enter no of integers:"))
+l=[];revl=[];ecount=0;ocount=0;s=0
+for x in range(n):
+    val=int(input("enter no:"))
+    l.append(val)
+for x in range(len(l)-1,-1,-1):
+    revl.append(l[x])
+Min=l[0];Max=l[0]
+for x in l:
+    s+=x
+    if x%2==0:
+        ecount+=1
+    else:
+        ocount+=1
+    if x<Min:
+        Min=x
+    elif x>Max:
+        Max=x
+    else:
+        continue
+print("Largest:",Max)
+print("Smallest:",Min)
+print("Sum:",s)
+print("Even Count:",ecount)
+print("Odd Count:",ocount)
+print("Reversed:",revl)
